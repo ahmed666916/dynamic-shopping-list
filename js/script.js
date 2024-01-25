c23ba9a21edd37261436138941c1f279359f90a9
ahmed666916 <ahmed666916@gmail.com>
2024-01-25 23:23:26 +0500

    added list_item, span and button in the onclick function

--- js/script.js
+++ js/script.js
@@ -9,4 +9,16 @@ btn.addEventListener("click", () => {
     let input_value = input.value;
     input.value = "";
 
+    const list_item = document.createElement('li');
+    list.appendChild(list_item);
+
+    const span = document.createElement('span');
+    span.textContent = input_value;
+    list_item.appendChild(span);
+
+    const button = document.createElement('button');
+    button.textContent = "Delete";
+    list_item.appendChild(button);
+
+
 })
